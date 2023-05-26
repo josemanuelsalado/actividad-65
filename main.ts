f6a8fc78e25e35207c4ffefb9b0b818d275a17ec
josemanuelsalado <132547206+josemanuelsalado@users.noreply.github.com>
2023-05-26 12:40:52 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,69 @@
-
+let d = 0
+basic.forever(function () {
+    d = input.compassHeading()
+    if (Math.abs(d - 45) < 22) {
+        basic.showLeds(`
+            # . . . .
+            . # . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (Math.abs(d - 45) < 22) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (Math.abs(d - 135) < 22) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . # . . .
+            # . . . .
+            `)
+    } else if (Math.abs(d - 180) < 22) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+    } else if (Math.abs(d - 225) < 22) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . # .
+            . . . . #
+            `)
+    } else if (0 < Math.abs(d - 270)) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # # #
+            . . . . .
+            . . . . .
+            `)
+    } else if (0 < Math.abs(d - 315)) {
+        basic.showLeds(`
+            . . . . #
+            . . . # .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
